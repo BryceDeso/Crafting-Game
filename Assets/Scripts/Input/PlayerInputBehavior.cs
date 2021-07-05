a5afc4fd47d427dc6269ff928ec932a3de605a5c
Bryce Deshotel <brycedeso123@gmail.com>
2021-07-05 16:02:28 -0500

    Added scripts, working on player movement
    Added scripts that will be used later on

---
+++ Assets/Scripts/Input/PlayerInputBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputBehavior : MonoBehaviour
+{
+    public PlayerController _playerControls;
+
+    private PlayerMovmentBehavior _playerMovement;
+
+    private void Awake()
+    {
+        _playerControls = new PlayerController();
+    }
+
+    private void OnEnable()
+    {
+        _playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerControls.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerMovement = GetComponent<PlayerMovmentBehavior>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 moveDirection = _playerControls.Player.Movement.ReadValue<Vector2>();
+        _playerMovement.Move(moveDirection);
+    }
+}
---
+++ Assets/Scripts/Movement/PlayerMovmentBehavior.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovmentBehavior : MonoBehaviour
+{
+    private PlayerInputBehavior _playerInput;
+
+    //A reference to the rigidbody component
+    private Rigidbody _rigidbody;
+
+    //A varibale that handles the movement speed of the player
+    public float moveSpeed;
+
+    //A reference of a Vector3
+    private Vector3 _velocity;
+
+    public Vector3 Velocity
+    {
+        get
+        {
+            return _velocity;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerInput = GetComponent<PlayerInputBehavior>();
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 direction)
+    {
+        _velocity = direction * moveSpeed * Time.deltaTime;
+    }
+
+    void FixedUpdate()
+    {
+        _rigidbody.MovePosition(transform.position + _velocity);
+    }
+}
