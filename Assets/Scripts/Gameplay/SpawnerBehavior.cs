07917e64ed870a560da515026b0026d472dfd099
Bryce Deshotel <brycedeso123@gmail.com>
2021-07-06 19:25:02 -0500

    Created Spawner

--- Assets/Scripts/Gameplay/SpawnerBehavior.cs
+++ Assets/Scripts/Gameplay/SpawnerBehavior.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class SpawnerBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Tooltip("Reference to the object that you want to spawn")]
+    [SerializeField]
+    private GameObject _spawnRef;
+    [Tooltip("The amount of time inbetween spawns")]
+    public float _timeInterval;
+
+    private float _maxTime;
+    //Used to tell how much time is left until the next spawn.
+    public float _timeLeft;
+
+    private void Start()
     {
-        
+        _maxTime = _timeInterval;
+        Instantiate(_spawnRef, transform.position, transform.rotation);
     }
 
-    // Update is called once per frame
+    // Start is called before the first frame update
     void Update()
     {
-        
+        Timer();
+    }
+
+    private void Timer()
+    {
+        _maxTime -= Time.deltaTime;
+        _timeLeft = _maxTime;
+
+        if (_timeLeft <= 0)
+        {
+            Instantiate(_spawnRef, transform.position, transform.rotation);
+
+            _maxTime = _timeInterval;
+        }
+
     }
 }
