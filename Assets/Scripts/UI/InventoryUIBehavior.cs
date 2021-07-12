cf3506d86a61c872ad6043f1f89a8fb2b934ca9f
Bryce <brycedeso123@gmail.com>
2021-07-12 17:16:09 -0500

    Adding inventory UI

---
+++ Assets/Scripts/UI/InventoryUIBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryUIBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerBehavior _player;
+
+    [SerializeField]
+    private Text iron;
+    [SerializeField]
+    private Text oakWood;
+    [SerializeField]
+    private Text weapon;
+    [SerializeField]
+    private Text pickaxe;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void UpdateUI()
+    {
+        iron.text = ("Iron held: " + _player._ironHeld);
+    }
+
+}
