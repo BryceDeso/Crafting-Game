3164e2665e212e3ca4ac0c6ac137d0189f0d4811
Bryce <brycedeso123@gmail.com>
2021-07-20 12:43:15 -0500

    Added healthbar and simple inventory
    Inventory will be fully updated later

--- Assets/Scripts/UI/InventoryUIBehavior.cs
+++ Assets/Scripts/UI/InventoryUIBehavior.cs
@@ -9,6 +9,9 @@ public class InventoryUIBehavior : MonoBehaviour
     private PlayerBehavior _player;
 
     [SerializeField]
+    private GameObject inventoryUI;
+
+    [SerializeField]
     private Text iron;
     [SerializeField]
     private Text oakWood;
@@ -17,22 +20,42 @@ public class InventoryUIBehavior : MonoBehaviour
     [SerializeField]
     private Text pickaxe;
 
+    private bool inventoryOpen;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        inventoryOpen = false;   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        OpenInventory();
+        UpdateUI();
+    }
+
+    private void OpenInventory()
+    {
+        if (Input.GetKeyDown(KeyCode.I) && inventoryOpen == false)
+        {
+            inventoryUI.SetActive(true);
+            inventoryOpen = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.I) && inventoryOpen == true)
+        {
+            inventoryUI.SetActive(false);
+            inventoryOpen = false;
+        }
     }
 
     private void UpdateUI()
     {
         iron.text = ("Iron held: " + _player._ironHeld);
+        oakWood.text = ("Oak Wood Held: " + _player._oakWoodHeld);
+        weapon.text = ("Weapon: " + _player._weapon);
+        pickaxe.text = ("Pickaxe: " + _player._pickaxe);
     }
 
 }
